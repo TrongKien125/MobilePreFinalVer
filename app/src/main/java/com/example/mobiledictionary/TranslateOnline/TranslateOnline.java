54ab48056bb6678dfd11a26d80d5abcfefd3a8d5
TrongKien125 <72129356+TrongKien125@users.noreply.github.com>
2022-01-12 22:26:30 +0700

    Hieucommit2

---
+++ app/src/main/java/com/example/mobiledictionary/TranslateOnline/TranslateOnline.java
@@ -0,0 +1,14 @@
+package com.example.mobiledictionary.TranslateOnline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TranslateOnline extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translate_online);
+    }
+}
